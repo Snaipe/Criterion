4dd9f77c41e7118944777ebdda09adbe03bf6070
Snaipe <franklinmathieu@gmail.com>
2015-02-02 13:02:59 +0100

    Added missing license headers

--- include/hooks.h
+++ include/hooks.h
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2015 Franklin "Snaipe" Mathieu <http://snai.pe/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #ifndef CRITERION_HOOKS_H_
 # define CRITERION_HOOKS_H_
 
@@ -26,8 +49,5 @@ typedef void (*f_report_hook)(void *);
     SECTION_("criterion_hooks_" #Kind)                                         \
     const f_report_hook HOOK_IDENTIFIER_(func) = HOOK_IDENTIFIER_(impl);      \
     HOOK_PROTOTYPE_
-
-extern __attribute__ ((weak)) void criterion_init(void);
-extern __attribute__ ((weak)) void criterion_fini(void);
 
 #endif /* !CRITERION_HOOKS_H_ */
