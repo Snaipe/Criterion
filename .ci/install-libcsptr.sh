09b7bdcd5a82ea2e2bb60041638edb346fe1ed10
Snaipe <franklinmathieu@gmail.com>
2015-04-15 13:34:34 +0200

    Added sudo-less install for cygwin builds

--- .ci/install-libcsptr.sh
+++ .ci/install-libcsptr.sh
@@ -9,6 +9,12 @@ git clone --branch ${tag} --depth 1 ${repo} dependencies/libcsptr &&
     ./autogen.sh &&
     ./configure --prefix=/usr &&
     make &&
-    sudo make install
+    {
+        if command -v sudo; then
+            sudo make install
+        else
+            make install
+        fi
+    }
 )
 rm -Rf dependencies
