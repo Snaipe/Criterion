71a9fdf67c8b9b74837d308dd0fb82b4ae783ac1
Snaipe <franklinmathieu@gmail.com>
2015-08-04 17:53:48 +0200

    Added more pattern tests

--- samples/tests/pattern.sh
+++ samples/tests/pattern.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 ./simple --pattern '*/passing'
+./simple --pattern '!(*/passing)'
+./simple --pattern '[pf]a@(ss|il)ing'
+./simple --pattern '@(+(nest)ed))'
+./simple --pattern '?(*(a|b))'
+./simple --pattern '?(malformed'
