880c75746f6d5d0f97bf6d0a48bfcfbfb047f18f
Snaipe <franklinmathieu@gmail.com>
2015-08-20 08:33:22 +0200

    Fixed comparison tests depending on absolute paths

--- samples/tests/run_test.sh
+++ samples/tests/run_test.sh
@@ -1,17 +1,24 @@
 #!/bin/sh -e
 
-if [ -f $1.in ]; then
-    sh -c "$1" > $1.out 2> $1.err < $1.in
+cmp_dir=$1; shift
+out_dir=$1; shift
+bin_dir=$1; shift
+
+mkdir -p $out_dir
+
+if [ -f $cmp_dir/$1.in ]; then
+    sh -c "$bin_dir/$*" > $out_dir/$1.out 2> $out_dir/$1.err < $cmp_dir/$1.in
 else
-    sh -c "$1" > $1.out 2> $1.err
+    mkdir -p $(dirname $out_dir/$1)
+    sh -c "$bin_dir/$*" > $out_dir/$1.out 2> $out_dir/$1.err
 fi
 
-if [ -f $2/$1.out.expected ] && [ "$(md5sum $1.out | cut -d' ' -f1)" != "$(md5sum $2/$1.out.expected | cut -d' ' -f1)" ]; then
-    diff $1.out $2/$1.out.expected
+if [ -f $cmp_dir/$1.out.expected ] && [ "$(md5sum $out_dir/$1.out | cut -d' ' -f1)" != "$(md5sum $cmp_dir/$1.out.expected | cut -d' ' -f1)" ]; then
+    diff $out_dir/$1.out $cmp_dir/$1.out.expected
     exit 255
 fi
 
-if [ -f $2/$1.err.expected ] && [ "$(md5sum $1.err | cut -d' ' -f1)" != "$(md5sum $2/$1.err.expected | cut -d' ' -f1)" ]; then
-    diff $1.err $2/$1.err.expected
+if [ -f $cmp_dir/$1.err.expected ] && [ "$(md5sum $out_dir/$1.err | cut -d' ' -f1)" != "$(md5sum $cmp_dir/$1.err.expected | cut -d' ' -f1)" ]; then
+    diff $out_dir/$1.err $cmp_dir/$1.err.expected
     exit 255
 fi
