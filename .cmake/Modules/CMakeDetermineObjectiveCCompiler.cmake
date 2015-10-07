baf93fb2f2f7bf431d068568ffda81e927b32b75
Snaipe <franklinmathieu@gmail.com>
2015-10-07 17:57:53 +0200

    Added CMAKE_PLATFORM_INFO_DIR definition for CMake <2.8.10 backward compatibility

--- .cmake/Modules/CMakeDetermineObjectiveCCompiler.cmake
+++ .cmake/Modules/CMakeDetermineObjectiveCCompiler.cmake
@@ -2,6 +2,11 @@
 # Redistribution and use of this file is allowed according to the terms of the MIT license.
 # For details see the LICENSE file distributed with Criterion.
 
+# CMake <2.8.10 backward compat
+if(NOT CMAKE_PLATFORM_INFO_DIR)
+  set(CMAKE_PLATFORM_INFO_DIR ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY})
+endif(NOT CMAKE_PLATFORM_INFO_DIR)
+
 set(CMAKE_ObjectiveC_COMPILER_ID ${CMAKE_C_COMPILER_ID})
 set(CMAKE_ObjectiveC_COMPILER ${CMAKE_C_COMPILER})
 
