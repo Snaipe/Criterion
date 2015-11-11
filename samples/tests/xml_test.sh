3e5f43ff74cd5099b8f76362e2de7838beacc851
Snaipe <franklinmathieu@gmail.com>
2015-11-11 13:52:22 +0100

    Fixed XML regression tests

--- samples/tests/xml_test.sh
+++ samples/tests/xml_test.sh
@@ -3,5 +3,5 @@
 ./signal.c.bin --xml --always-succeed
 ./asserts.c.bin --xml --always-succeed
 ./more-suites.c.bin --xml --always-succeed
-./long-messages.c.bin --xml --always-succeed
+./tests/long-messages.c.bin --xml --always-succeed
 ./description.c.bin --xml --always-succeed
