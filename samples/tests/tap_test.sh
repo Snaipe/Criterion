c77ec236780a36dc0f146d87af21515a5efd24ee
Snaipe <franklinmathieu@gmail.com>
2015-11-12 23:37:13 +0100

    Fixed regression test for TAP output format

--- samples/tests/tap_test.sh
+++ samples/tests/tap_test.sh
@@ -3,5 +3,5 @@
 ./signal.c.bin --tap --always-succeed
 ./asserts.c.bin --tap --always-succeed
 ./more-suites.c.bin --tap --always-succeed
-./long-messages.c.bin --tap --always-succeed
+./tests/long-messages.c.bin --tap --always-succeed
 ./description.c.bin --tap --always-succeed
