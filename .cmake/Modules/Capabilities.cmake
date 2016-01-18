dc62c45c0829f23d4c3a770dead25083d624e7dd
Snaipe <franklinmathieu@gmail.com>
2016-01-18 14:16:13 +0100

    [Issue #96] Refactored the build system configuration

--- .cmake/Modules/Capabilities.cmake
+++ .cmake/Modules/Capabilities.cmake
@@ -1,8 +1,25 @@
-# Copyright (C) 2015 Franklin "Snaipe" Mathieu.
+# Copyright (C) 2015-2016 Franklin "Snaipe" Mathieu.
 # Redistribution and use of this file is allowed according to the terms of the MIT license.
 # For details see the LICENSE file distributed with Criterion.
 
 include(CheckPrototypeDefinition)
+include(CheckLibraryExists)
+include(PackageUtils)
+
+# Check for packages
+
+cr_find_package (Gettext)
+cr_find_package (Libintl)
+
+if (I18N AND GETTEXT_FOUND AND LIBINTL_LIB_FOUND)
+  set (GettextTranslate_ALL 1)
+  set (GettextTranslate_GMO_BINARY 1)
+  set (ENABLE_NLS 1)
+endif ()
+
+cr_find_package (PCRE PKGCONFIG libpcre)
+
+# Check for functions
 
 check_prototype_definition(
   strtok_s
@@ -10,3 +27,31 @@ check_prototype_definition(
   NULL
   "string.h"
   HAVE_STRTOK_S)
+
+check_library_exists (rt clock_gettime "time.h" HAVE_CLOCK_GETTIME)
+
+# Check for C++11
+
+if (LANG_CXX)
+  enable_language(CXX)
+endif ()
+
+if (NOT MSVC AND CMAKE_CXX_COMPILER_WORKS)
+  include(CheckCXXCompilerFlag)
+  CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+  CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+
+  if(COMPILER_SUPPORTS_CXX11)
+    set(CXX11_FLAG "-std=c++11")
+  elseif(COMPILER_SUPPORTS_CXX0X)
+    set(CXX11_FLAG "-std=c++0x")
+  else()
+    message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
+  endif()
+endif()
+
+# Valgrind support
+
+if (DEV_BUILD)
+  set(ENABLE_VALGRIND_ERRORS 1)
+endif ()
---
+++ .cmake/Modules/Properties.cmake
@@ -0,0 +1,35 @@
+# Copyright (C) 2015-2016 Franklin "Snaipe" Mathieu.
+# Redistribution and use of this file is allowed according to the terms of the MIT license.
+# For details see the LICENSE file distributed with Criterion.
+
+set (PROJECT_VERSION "2.2.0")
+set (PROJECT_SOVERSION 3)
+set (LOCALEDIR_REL "share/locale")
+set (LOCALEDIR "${CMAKE_INSTALL_PREFIX}/${LOCALEDIR_REL}")
+
+add_definitions(
+  -DCRITERION_BUILDING_DLL=1
+  -DPB_ENABLE_MALLOC=1
+)
+
+set (CMAKE_C_FLAGS_DEFAULT "${CMAKE_C_FLAGS}")
+set (CMAKE_CXX_FLAGS_DEFAULT "${CMAKE_CXX_FLAGS}")
+
+if (MSVC)
+  set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /SAFESEH:NO")
+  add_definitions (-D_CRT_SECURE_NO_WARNINGS=1)
+else ()
+  if (WIN32)
+    set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-no-undefined")
+  endif ()
+
+  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -g -std=gnu99")
+  if (CMAKE_CXX_COMPILER_WORKS)
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -g ${CXX11_FLAG}")
+  endif ()
+endif ()
+
+include_directories(SYSTEM
+  /usr/local/include
+  /usr/include/GNUstep
+)
