cc9465e133671bb11e425eced6c94e93fb59b7f7
Snaipe <franklinmathieu@gmail.com>
2016-01-20 14:49:15 +0100

    [samples] Fixed sample with a cross platform segfault function

--- samples/signal.c
+++ samples/signal.c
@@ -1,14 +1,33 @@
 #include <signal.h>
 #include <criterion/criterion.h>
 
+void crash(void);
+
 Test(simple, caught, .signal = SIGSEGV) {
-    raise(SIGSEGV);
+    crash();
 }
 
 Test(simple, wrong_signal, .signal = SIGINT) {
-    raise(SIGSEGV);
+    crash();
 }
 
 Test(simple, uncaught) {
+    crash();
+}
+
+
+// Cross platform segfault simulator ™
+// a.k.a. "I can't believe I have to write this for a sample"
+
+#ifdef _WIN32
+# include <windows.h>
+#endif
+
+void crash(void) {
+#ifdef _WIN32
+    // This translates to a SIGSEGV
+    RaiseException(EXCEPTION_ACCESS_VIOLATION, EXCEPTION_NONCONTINUABLE, 0, NULL);
+#else
     raise(SIGSEGV);
+#endif
 }
--- test/cram/tap.t
+++ test/cram/tap.t
@@ -17,7 +17,7 @@ Testing multiple samples with --tap
   
   # Running 3 tests from simple
   not ok - simple::wrong_signal 
-  not ok - simple::uncaught unexpected signal after signal.c:12
+  not ok - simple::uncaught unexpected signal after signal.c:14
   ok - simple::caught 
 
   $ asserts.c.bin --tap
