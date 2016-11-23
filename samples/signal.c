0f3142cf8ed1ca01236ff230484a6acbcbf6b59f
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2016-11-23 02:44:03 +0100

    samples: Simplified signal samples

--- samples/signal.c
+++ samples/signal.c
@@ -15,19 +15,9 @@ Test(simple, uncaught) {
     crash();
 }
 
-/* Cross platform segfault simulator ™
-   a.k.a. "I can't believe I have to write this for a sample" */
-
-#ifdef _WIN32
-# include <windows.h>
-#endif
-
 void crash(void)
 {
-#ifdef _WIN32
-    /* This translates to a SIGSEGV */
-    RaiseException(EXCEPTION_ACCESS_VIOLATION, EXCEPTION_NONCONTINUABLE, 0, NULL);
-#else
-    raise(SIGSEGV);
-#endif
+    volatile int *ptr = NULL;
+
+    *ptr = 42;
 }
