7d23251fbb2df81f1cf9b8628e52e3bf7738d34c
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2017-05-01 22:28:03 +0100

    sphinx: fixed the docs

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import re
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
