f09a294a07c3916e666a379169168bad022e9e83
Franklin "Snaipe" Mathieu <me@snai.pe>
2019-09-14 19:14:47 +0100

    meson: remove _GNU_SOURCE defines as meson handles that

--- src/io/tap.c
+++ src/io/tap.c
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/log/normal.c
+++ src/log/normal.c
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#define _GNU_SOURCE
 #define CRITERION_LOGGING_COLORS
 #include <stdio.h>
 #include <stdlib.h>
