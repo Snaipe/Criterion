81982a28a4bc2547046624cb5f37182680f5fd1a
Karim DRIDI <seth.holth@gmail.com>
2019-10-31 14:33:47 +0100

    Update the share directory location
    This location depends on the initial install directory of automake. Eg, it can be installed inside /usr/local instead of /usr

--- dev/autotools/autogen.sh
+++ dev/autotools/autogen.sh
@@ -2,9 +2,10 @@
 
 # copy TAP driver into build-aux
 automake_ver=$(automake --version | \grep -E -o '[0-9]\.[0-9]{2}')
+automake_dir=$(dirname $(which automake))/..
 
 mkdir -p build-aux
-cp -f /usr/share/automake-$automake_ver/tap-driver.sh build-aux
+cp -f $automake_dir/share/automake-$automake_ver/tap-driver.sh build-aux
 
 # create criterion TAP log compiler
 # this is necessary to print TAP (and only TAP) on the standard output,
