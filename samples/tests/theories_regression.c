7ee235fe3cc0d308ba331248d09b01941f86386c
Franklin "Snaipe" Mathieu <me@snai.pe>
2019-12-28 21:07:04 +0000

    theories: switch from dyncall to libffi
    As much as I like dyncall, it just isn't that broadly available on linux
    distros. Using libffi has the advantage of being ubiquitous and targets
    all of our platforms.
    
    So long, and thanks for all the fish.

--- samples/tests/theories_regression.c
+++ samples/tests/theories_regression.c
@@ -13,22 +13,25 @@ TheoryDataPoints(theory, misc) = {
     DataPoints(int,          1),
     DataPoints(float,        3.14f),
     DataPoints(double,       3.14),
+    DataPoints(long double,  3.14l),
     DataPoints(char *,       "test"),
     DataPoints(const char *, "other test"),
 };
 
-Theory((char c, bool b, short s, int i, float f, double d, char *str, const char *cstr), theory, misc) {
+Theory((char c, bool b, short s, int i, float f, double d, long double ld, char *str, const char *cstr), theory, misc) {
     float reff = 3.14f;
     double refd = 3.14;
+    long double refld = 3.14l;
 
-    cr_assert(b);
-    cr_assert_eq(c, 'a');
-    cr_assert_eq(s, 1);
-    cr_assert_eq(i, 1);
-    cr_assert_eq(f, reff);
-    cr_assert_eq(d, refd);
-    cr_assert_str_eq(str, "test");
-    cr_assert_str_eq(cstr, "other test");
+    cr_expect(b);
+    cr_expect_eq(c, 'a');
+    cr_expect_eq(s, 1);
+    cr_expect_eq(i, 1);
+    cr_expect_eq(f, reff);
+    cr_expect_eq(d, refd);
+    cr_expect_eq(ld, refld);
+    cr_expect_str_eq(str, "test");
+    cr_expect_str_eq(cstr, "other test");
 
     /* abort to see the formatted string of all parameters */
     cr_assert_fail();
--- test/cram/theories.t
+++ test/cram/theories.t
@@ -1,11 +1,11 @@
 Using theories
 
   $ theories_regression.c.bin
-  [----] theories_regression.c:34: Assertion Failed
+  [----] theories_regression.c:37: Assertion Failed
   [----]   
   [----]   The conditions for this assertion were not met.
   [----]   
-  [----]   Theory theory::misc failed with the following parameters: ('a', true, 1, 1, 3.14f, 3.14, "test", "other test")
+  [----]   Theory theory::misc failed with the following parameters: ('a', true, 1, 1, 3.14f, 3.14, 3.14l, "test", "other test")
   [FAIL] theory::misc
   [====] Synthesis: Tested: 1 | Passing: 0 | Failing: 1 | Crashing: 0 
 
