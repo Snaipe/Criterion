66ef1969860994f94190a85af9b76f17674ea70f
Romain DÉOUX <romain.deoux@proton.me>
2024-08-18 11:28:13 +0200

    fix: allow asserting pointers to const data

--- test/full/bug521.c
+++ test/full/bug521.c
@@ -2,13 +2,18 @@
 #include <criterion/new/assert.h>
 
 Test(bug521, str) {
-  static const char *foo = "foo";
+  static const char *const foo = "foo";
   cr_assert(eq(str, foo, "foo"));
   cr_assert(ne(str, foo, "oof"));
 }
 
 Test(bug521, wcs) {
-  static const wchar_t *foo = L"foo";
+  static const wchar_t *const foo = L"foo";
   cr_assert(eq(wcs, foo, L"foo"));
   cr_assert(ne(wcs, foo, L"oof"));
+}
+
+Test(bug521, ptr) {
+  static const char *const foo = "foo";
+  cr_assert(eq(ptr, foo, foo));
 }
